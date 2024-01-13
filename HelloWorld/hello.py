95c7b4d239646c27c44ba55bd4131d27042f83ee
Ashley DeMott <ashley.demott28@gmail.com>
2024-01-13 10:57:38 -0700

    initial files

---
+++ HelloWorld/hello.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+
+text = "Hello again"
+print(text)
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Ashley DeMott
+ * Hello World - Java
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
